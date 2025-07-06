37ed8d228c12f776fb848e0d4438b907953588c6
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-07-06 15:49:10 +0900

    feat: standardize api response

---
+++ bank-api/src/main/kotlin/common/ApiResponse.kt
@@ -0,0 +1,36 @@
+package com.example.bank.common
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.sun.net.httpserver.Authenticator
+import org.springframework.http.ResponseEntity
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class ApiResponse<T>(
+    val success: Boolean,
+    val message: String,
+    val data: T? = null,
+    val err: Error? = null
+) {
+    companion object {
+        fun <T> success(data: T, message: String): ResponseEntity<ApiResponse<T>> {
+            return ResponseEntity.ok(ApiResponse(true, message, data))
+        }
+
+        fun <T> error(
+            message: String,
+            errCode: String? = null,
+            details: Any? = null,
+            path: String? = null
+        ): ResponseEntity<ApiResponse<T>> {
+            return ResponseEntity.badRequest().body(
+                ApiResponse(false, message, null, Error(errCode, details, path))
+            )
+        }
+    }
+}
+
+data class Error (
+    val code: String? = null,
+    val details: Any? = null,
+    val path: String? = null
+)

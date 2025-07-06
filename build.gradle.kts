7318a7267e7ab69b67e5c0c105c0f43fb0bddc80
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-07-06 13:32:52 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    kotlin("jvm") version "1.9.25" apply false
+    kotlin("plugin.spring") version "1.9.25" apply false
+    kotlin("plugin.jpa") version "1.9.25" apply false
+    id("org.springframework.boot") version "3.5.3" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+}
+
+allprojects {
+    group = "org.ktor_lecture"
+    version = "1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "io.spring.dependency-management")
+
+    if (name == "bank-api") {
+        apply(plugin = "org.springframework.boot")
+        apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+        apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    }
+
+    if (name == "bank-core") {
+        apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    }
+
+    if (name == "bank-domain") {
+        apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    }
+
+    if (name == "bank-event") {
+        apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    }
+
+    the<io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension>().apply {
+        imports {
+            mavenBom("org.springframework.boot:spring-boot-dependencies:3.2.3")
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+            freeCompilerArgs += "-Xjsr305=strict"
+        }
+    }
+}
+
---
+++ src/test/kotlin/com/example/multimodulebank/MultiModuleBankApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.multimodulebank
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MultiModuleBankApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

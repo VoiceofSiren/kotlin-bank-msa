2c4686c78e939844856826f0ad4e40dbb0a26106
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-07-12 14:30:34 +0900

    gradle: new dependencies of bank-monitoring module, circuit breaker and jpa

--- bank-api/build.gradle.kts
+++ bank-api/build.gradle.kts
@@ -3,6 +3,15 @@ dependencies {
     implementation(project(":bank-core"))
     implementation(project(":bank-domain"))
     implementation(project(":bank-event"))
+    implementation(project(":bank-monitoring"))
+
+    // circuit
+    implementation("io.github.resilience4j:resilience4j-spring-boot2:2.0.2")
+    implementation("io.github.resilience4j:resilience4j-circuitbreaker:2.0.2")
+    implementation("io.github.resilience4j:resilience4j-retry:2.0.2")
+
+    //jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     // swagger
     // http://localhost:<port>/swagger-ui/index.html

4c0bff6f5f8de9e81d3644ac6235ae25cbdfa3a2
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-07-15 15:33:41 +0900

    fix: solve type mismatch

--- bank-domain/src/main/kotlin/repository/AccountRepository.kt
+++ bank-domain/src/main/kotlin/repository/AccountRepository.kt
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface AccountRepository: JpaRepository<Account, Long> {
-    fun findByAccountNumber(accountNumber: Long): Account?
+    fun findByAccountNumber(accountNumber: String): Account?
 }

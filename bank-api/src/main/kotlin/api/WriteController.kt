d0cc5a305bf0961a2240dc25f1bc16de6204fffd
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-07-18 11:59:24 +0900

    feat: define write-only api

--- bank-api/src/main/kotlin/api/WriteController.kt
+++ bank-api/src/main/kotlin/api/WriteController.kt
@@ -1,4 +1,77 @@
 package com.example.bank.api
 
-class WriteController {
+import com.example.bank.common.ApiResponse
+import com.example.bank.domain.dto.AccountView
+import com.example.bank.service.AccountWriteService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.math.BigDecimal
+import io.swagger.v3.oas.annotations.responses.ApiResponse as SwaggerApiResponse
+
+data class CreateAccountRequest (
+    val name : String,
+    val initialBalance : BigDecimal,
+)
+
+@RestController
+@RequestMapping("/api/v1/write")
+@Tag(name = "Write API", description = "Write Operation")
+class WriteController (
+    private val accountWriteService: AccountWriteService,
+) {
+    private val logger = LoggerFactory.getLogger(WriteController::class.java)
+
+    @Operation(
+        summary = "Create new account",
+        description = "Create a new account with specified account holder name and initial balance"
+    )
+    @PostMapping
+    fun createAcount(
+        @RequestBody request: CreateAccountRequest
+    ): ResponseEntity<ApiResponse<AccountView>> {
+        logger.info("Creates account for: ${request.name} with initial balance: ${request.initialBalance}")
+        return accountWriteService.createAccount(request.name, request.initialBalance)
+    }
+
+    @Operation(
+        summary = "Transfer money from one account to another",
+        description = "Transfers the specified amount of money from one account to another",
+        responses = [
+            SwaggerApiResponse(
+                responseCode = "200",
+                description = "Transfer Successfully"
+            ),
+            SwaggerApiResponse(
+                responseCode = "400",
+                description = "Invalid amount or insufficient funds"
+            ),
+            SwaggerApiResponse(
+                responseCode = "404",
+                description = "Account Not Found"
+            )
+        ]
+    )
+    @PostMapping("/transfer")
+    fun transfer(
+        @Parameter(description = "Source account number", required = true)
+        @RequestParam fromAccountNumber: String,
+        @Parameter(description = "Destination account number", required = true)
+        @RequestParam toAccountNumber: String,
+        @Parameter(description = "Amount to transfer", required = true)
+        @RequestParam amount: BigDecimal,
+    ): ResponseEntity<ApiResponse<String>> {
+        logger.info("Transfer money from $fromAccountNumber to $toAccountNumber with amount: $amount")
+        return accountWriteService.transfer(fromAccountNumber, toAccountNumber, amount)
+    }
+
+
+
 }
